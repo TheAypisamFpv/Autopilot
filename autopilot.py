db20c4a48f8b918117d5b6c7cd07836e692e287e
Aypisam <the.aypisam.fpv@gmail.com>
2023-03-22 02:30:12 +0100

    autocommit: 1 files changed, 1 insertions(+), 1 deletions(-)

--- autopilot.py
+++ autopilot.py
@@ -2,6 +2,19 @@ import cv2 as cv
 import numpy as np
 
 
+height = 720
+width = 1280
+
+center = int(width/2)
+car_hood = 150
+car_width = 900
+distance_center = int(width/2)
+detection_distance = 120
+distance_width = 130
+
+
+
+
 def display_lines(frame, lines):
     line_image = np.zeros_like(frame)
     if lines is not None:
@@ -17,14 +30,13 @@ while (cap.isOpened()):
     success, frame = cap.read()
     displayed_frame = frame.copy()
     """rbg2gray and transform the view to bird eye view"""
-    height = 720
-    width = 1280
+
 
     # transform the view to bird eye view
-    top_left = [560, 450]
-    top_right = [750, 450]
-    bottom_left = [250, height-70]
-    bottom_right = [width-150, height-70]
+    top_left = [int(distance_center - distance_width/2), int(height - car_hood - detection_distance)]
+    top_right = [int(distance_center + distance_width/2), int(height - car_hood - detection_distance)]
+    bottom_left = [int(center - car_width/2), int(height-car_hood)]
+    bottom_right = [int(center + car_width/2), int(height-car_hood)]
 
     cv.circle(displayed_frame, top_left, 5, (0, 0, 255), -1)
     cv.circle(displayed_frame, top_right, 5, (0, 0, 255), -1)
@@ -42,24 +54,29 @@ while (cap.isOpened()):
     matrix = cv.getPerspectiveTransform(pts1, pts2)
     transformed_frame = cv.warpPerspective(frame_gray, matrix, (width, height))
 
-    kernel = np.array([[0, -1, 0],
-                       [-1, 5, -1],
-                       [0, -1, 0]])
-    frame_sharpen = cv.filter2D(transformed_frame, -1, kernel)
-
+    kernel_sharpen = np.array([[0, -1, 0],
+                               [-1, 5, -1],
+                               [0, -1, 0]])
+    frame_sharpen = cv.filter2D(transformed_frame, -1, kernel_sharpen)
+    # frame_sharpen = cv.filter2D(frame_sharpen, -1, kernel_sharpen)
 
     alpha = 2  # Contrast control (1.0-3.0)
     beta = -200  # Brightness control (0-100)
     adjusted = cv.convertScaleAbs(frame_sharpen, alpha=alpha, beta=beta)
 
+    # kernel_edge = np.array([[0, -1, 0],
+    #                         [-1, 5, -1],
+    #                         [0, -1, 0]])
+    # edge_detection = cv.filter2D(adjusted, -1, kernel_edge)
 
-    frame_canny = cv.Canny(adjusted, 100, 150)
-    lines = cv.HoughLinesP(frame_canny, 1, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=15)
+    frame_canny = cv.Canny(adjusted, 100, 250)
+    lines = cv.HoughLinesP(frame_canny, 1, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=30)
     frame_lines = display_lines(frame_canny, lines)
-    combo_image = cv.addWeighted(frame_lines, 0.8, frame_lines, 1, 1)
+    combo_image = cv.addWeighted(adjusted, 0.8, frame_lines, 1, 1)
 
     cv.imshow('adjusted', adjusted)
-    cv.imshow('combo_image', combo_image)
+    cv.imshow('frame_canny', frame_canny)
+    cv.imshow('combo_image', frame_lines)
     if cv.waitKey(1) == 27:
         break
 
