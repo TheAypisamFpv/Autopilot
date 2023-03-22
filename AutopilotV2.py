0a4bffb6bca6b4df29b5a2d2945eeaa6cd052d7f
the aypisam fpv <the.aypisam.64@gmail.com>
2023-03-22 17:40:55 +0100

    works better

--- AutopilotV2.py
+++ AutopilotV2.py
@@ -3,34 +3,40 @@ import numpy as np
 import math
 
 
+AVAILABLE_COLOR = (0, 255, 0)
+UNAVAILABLE_COLOR = (0, 0, 255)
+autopilot_available = 0
+
+
 height = 720
 width = 1280
 
-center = int(width/2)
+center = int(width/2)-70
 car_hood = 150
-car_width = 900
-distance_center = int(width/2)
-detection_distance = 120
-distance_width = 130
+car_width = 1000
+distance_center = center-20
+detection_distance = 175
+distance_width = 125
 
 
 
+left_detect_lane_pos = 250
+right_detect_lane_pos = width-260
 
-left_detect_lane_pos = 330
-right_detect_lane_pos = 1022
+lane_detect_width = 125
+upper_lower_separation = 0
+upper_x_offset = 0
+lane_detect_height = 450
 
-lane_detect_width = 100
-lane_detect_height = 250
-
-left_look_at_upper_pt1 = [left_detect_lane_pos - lane_detect_width, height - lane_detect_height]
-left_look_at_upper_pt2 = [left_detect_lane_pos + lane_detect_width, height - int(lane_detect_height/2)]
+left_look_at_upper_pt1 = [left_detect_lane_pos - lane_detect_width + upper_x_offset, height - lane_detect_height - upper_lower_separation]
+left_look_at_upper_pt2 = [left_detect_lane_pos + lane_detect_width + upper_x_offset, height - int(lane_detect_height/2) - upper_lower_separation]
 
 left_look_at_lower_pt1 = [left_detect_lane_pos - lane_detect_width, height - int(lane_detect_height/2)]
 left_look_at_lower_pt2 = [left_detect_lane_pos + lane_detect_width, height]
 
 
-right_look_at_upper_pt1 = [right_detect_lane_pos - lane_detect_width, height - lane_detect_height]
-right_look_at_upper_pt2 = [right_detect_lane_pos + lane_detect_width, height - int(lane_detect_height/2)]
+right_look_at_upper_pt1 = [right_detect_lane_pos - lane_detect_width - upper_x_offset, height - lane_detect_height - upper_lower_separation]
+right_look_at_upper_pt2 = [right_detect_lane_pos + lane_detect_width - upper_x_offset, height - int(lane_detect_height/2) - upper_lower_separation]
 
 right_look_at_lower_pt1 = [right_detect_lane_pos - lane_detect_width, height - int(lane_detect_height/2)]
 right_look_at_lower_pt2 = [right_detect_lane_pos + lane_detect_width, height]
@@ -65,7 +71,7 @@ def detect_lines(frame):
 
 
     """look at white pixel around left_look_at"""
-    thresh = 200
+    thresh = 245
     frame_wb = cv.threshold(frame, thresh, 255, cv.THRESH_BINARY)[1]
 
     left_upper_bbox = left_look_at_upper_pt1 + left_look_at_upper_pt2
@@ -168,12 +174,16 @@ def detect_lines(frame):
     return displayed_wb, left_angle, right_angle, detected
 
 
-
-cap = video_capture = cv.VideoCapture('project_video.mp4')
+"""
+Video here
+"""
+cap = video_capture = cv.VideoCapture('VID_2.mp4')
 
 left_pre_angle = 0
 right_pre_angle = 0
 true_pre_angle = 0
+last_good_angle = 0
+pre_autopilot_available = 0
 
 while (cap.isOpened()):
     success, frame = cap.read()
@@ -187,11 +197,6 @@ while (cap.isOpened()):
                  int(height - car_hood - detection_distance)]
     bottom_left = [int(center - car_width/2), int(height-car_hood)]
     bottom_right = [int(center + car_width/2), int(height-car_hood)]
-
-    cv.circle(displayed_frame, top_left, 5, (0, 0, 255), -1)
-    cv.circle(displayed_frame, top_right, 5, (0, 0, 255), -1)
-    cv.circle(displayed_frame, bottom_left, 10, (0, 0, 255), -1)
-    cv.circle(displayed_frame, bottom_right, 10, (0, 0, 255), -1)
     
 
     pts1 = np.float32([top_left, bottom_left, top_right, bottom_right])
@@ -209,7 +214,7 @@ while (cap.isOpened()):
     # frame_sharpen = cv.filter2D(frame_sharpen, -1, kernel_sharpen)
 
     alpha = 3  # Contrast control (1.0-3.0)
-    beta = -200  # Brightness control (0-100)
+    beta = -100  # Brightness control (0-100)
     adjusted = cv.convertScaleAbs(frame_sharpen, alpha=alpha, beta=beta)
 
     lanes, left_line_angle, right_line_angle, detected = detect_lines(adjusted)  # detected: 0:None , 1:left, 2:right, 3,both
@@ -220,8 +225,8 @@ while (cap.isOpened()):
 
 
 
-    left_line_angle = ((left_line_angle*1 + left_pre_angle*2)/3)
-    right_line_angle = ((right_line_angle*1 + right_pre_angle*2)/3)
+    left_line_angle = ((left_line_angle*1 + left_pre_angle*5)/6)
+    right_line_angle = ((right_line_angle*1 + right_pre_angle*5)/6)
 
 
     """
@@ -237,24 +242,6 @@ while (cap.isOpened()):
 
     true_angle = ((left_line_angle*left_certitude) + (right_line_angle*right_certitude))/2
 
-
-    # if left_line_angle_delta > limit and right_line_angle_delta < limit:
-    #     line_focus = 2
-    #     true_angle = [right_line_angle]
-    # elif left_line_angle_delta < limit and right_line_angle_delta > limit:
-    #     line_focus = 1
-    #     true_angle = [left_line_angle]
-    # elif left_line_angle_delta > limit and right_line_angle_delta > limit:
-    #     line_focus = 0
-    #     true_angle = [true_pre_angle]
-    # else:
-    #     line_focus = 3
-    #     true_angle = [left_line_angle, right_line_angle]
-
-    
-
-
-
     left_arrow_start = (left_detect_lane_pos, height)
     left_arrow_end = (left_detect_lane_pos + int(left_detect_lane_pos * math.sin(left_line_angle/180*math.pi)) , int(height - lane_detect_height*left_certitude))
 
@@ -262,20 +249,56 @@ while (cap.isOpened()):
     right_arrow_end = (right_detect_lane_pos + int(right_detect_lane_pos * math.sin(right_line_angle/180*math.pi)) ,int(height - lane_detect_height*right_certitude))
 
     # true_angle = np.mean(true_angle)
-    true_angle = ((true_angle*1 + true_pre_angle*5)/6)
+    true_angle = ((true_angle*1 + true_pre_angle*30)/31)
 
-    true_arrow_start = (int(width/2), height)
-    true_arrow_end = (int(width/2) + int(width/2 * math.sin(true_angle/180*math.pi)), height - lane_detect_height)
+    if left_certitude > 0.6 or right_certitude > 0.6:
+        last_good_angle = true_pre_angle
+        autopilot_available = 1
 
-    print(
-        f"lanes angle: {left_line_angle}° -- {right_line_angle}°    -----   {left_line_angle_delta} ({left_certitude})   {right_line_angle_delta} ({right_certitude})")
+    elif left_certitude < 0.4 and right_certitude < 0.4:
+        true_angle = last_good_angle
+        autopilot_available = 0
+
+
+
+    autopilot_available = (autopilot_available*1 + pre_autopilot_available*10)/11
+
+    if autopilot_available > 0.7:
+        displayed_color = AVAILABLE_COLOR
+        """display 'autopilot Available' on "displayed_frame"""
+        cv.putText(displayed_frame, 'Autopilot Available', (int(width/2)-150, 30), cv.FONT_HERSHEY_SIMPLEX, 1, displayed_color, 2, cv.LINE_AA)
+    else:
+        displayed_color = UNAVAILABLE_COLOR
+        """display 'autopilot Unavailable' on "displayed_frame"""
+        cv.putText(displayed_frame, 'Autopilot Unavailable', (int(width/2)-160, 30), cv.FONT_HERSHEY_SIMPLEX, 1, displayed_color, 2, cv.LINE_AA)
+
+
+    true_arrow_start = (center, height)
+    true_arrow_end = (distance_center + int(distance_center * math.sin(true_angle/180*math.pi)), height - lane_detect_height)
+
+    absolute_arrow_lenght = 75
+    absolute_arrow_start = (int(width/2), absolute_arrow_lenght+50)
+    absolute_arrow_end = (int(width/2) + int(absolute_arrow_lenght * math.sin(true_angle*4/180*math.pi)), absolute_arrow_start[1] - int(absolute_arrow_lenght * math.cos(true_angle*4/180*math.pi)))
+
+
+
+
+    """
+    Display
+    """
+
+    print(f"Autopilot available: {autopilot_available > 0.7} ({autopilot_available:.2f}) last_good_angle: {last_good_angle:.3f} {displayed_color == AVAILABLE_COLOR}  lanes angle: {left_line_angle:.3f}° -- {right_line_angle:.3f}° --> {true_angle:.3f}°   -----   {left_line_angle_delta:.3f} ({left_certitude:.3f})   {right_line_angle_delta:.3f} ({right_certitude:.3f})")
 
     cv.line(adjusted, left_arrow_start, left_arrow_end, (0,0,0), 5)
     cv.line(adjusted, right_arrow_start, right_arrow_end, (0, 0, 0), 5)
     cv.line(adjusted, true_arrow_start, true_arrow_end, (0, 0, 0), 7)
 
-    
-    cv.line(displayed_frame, true_arrow_start, true_arrow_end, (0, 0, 255), 7)
+    cv.line(displayed_frame, absolute_arrow_end, absolute_arrow_start, displayed_color, 7)
+    cv.line(displayed_frame, true_arrow_start, true_arrow_end, displayed_color, 7)
+    cv.circle(displayed_frame, top_left, 5, displayed_color, -1)
+    cv.circle(displayed_frame, top_right, 5, displayed_color, -1)
+    cv.circle(displayed_frame, bottom_left, 10, displayed_color, -1)
+    cv.circle(displayed_frame, bottom_right, 10, displayed_color, -1)
 
     cv.imshow('displayed_frame', displayed_frame)
     cv.imshow('WB', lanes)
@@ -286,4 +309,5 @@ while (cap.isOpened()):
     left_pre_angle = left_line_angle
     right_pre_angle = right_line_angle
     true_pre_angle = true_angle
-    cv.waitKey(10)
+    pre_autopilot_available = autopilot_available
+    cv.waitKey(1)
